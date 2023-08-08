94e9a4b619d291f2e668c14267eea15ec1a28bc7
Yunat Amos <57252898+yunatamos@users.noreply.github.com>
2023-08-08 11:14:47 +0300

    Update add.sh

--- add.sh
+++ add.sh
@@ -1,1 +1,9 @@
 
+ssh-keygen -t ed25519 -C "your_email@example.com"
+
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_ed25519
+
+sudo apt-get install xclip
+xclip -sel clip < ~/.ssh/id_ed25519.pub
+ssh -T git@github.com
